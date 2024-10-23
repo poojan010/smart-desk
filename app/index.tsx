3e8674a0342e6cfd4507f6c79fe7295e17535d23
Poojan Bhatt <pdbhatt010@gmail.com>
2024-10-23 08:20:47 +0530

    adding jest and eas configs

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
     </View>
-  );
+  )
 }
+
+export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
